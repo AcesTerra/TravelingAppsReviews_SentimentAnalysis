9b7322a478397b02424078499a557055142fbf1a
AcesTerra <38500428+AcesTerra@users.noreply.github.com>
2022-12-06 10:40:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,7 @@ class SentimentClassifier(nn.Module):
   def forward(self, input_ids, attention_mask):
     _, pooled_output = self.bert(
       input_ids=input_ids,
-      attention_mask=attention_mask,
-      return_dict=False
+      attention_mask=attention_mask
     )
     output = self.drop(pooled_output)
     return self.out(output)
